9c9872178bdaf07b0959f302879aaf25965eefda
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-08-14 16:38:03 -0400

    feat: Add links to homepage, shop and cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return(
+    return (
       <div>
-        <h2>My Shop</h2>
+        <h2><Link to="/">Buyable</Link></h2>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="cart">Cart</Link>
+          </li>
+        </ul>
       </div>
-    )
+    );
   }
 
 export default Navbar;
