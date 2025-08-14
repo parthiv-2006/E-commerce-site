423a3e90dfe9716831b0733973a5e1ff25b16b0c
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-08-14 16:53:11 -0400

    feat: fetch fake shopping api data and display titles of items

--- src/components/ShoppingPage.jsx
+++ src/components/ShoppingPage.jsx
@@ -1,10 +1,51 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function ShoppingPage() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <div>
-      <h1>Shop</h1>
-      <p>Find whatever your heart desires</p>
+      <div>
+        <h1>Shop</h1>
+        <p>Find whatever your heart desires</p>
+      </div>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
